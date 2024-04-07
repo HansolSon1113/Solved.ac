174a763cab4c988a092a0697f5ca1a2135858707
Hansol_Son <hansol@hansung.ac.kr>
2024-04-07 12:05:48 +0900

    불필요한 배열 제거

--- BJ/main.c
+++ BJ/main.c
@@ -1,3 +1,4 @@
+//1598
 #include <stdio.h>  //stdio.h 헤더 파일
 #include <stdlib.h>
 
@@ -16,34 +17,27 @@ void printForDebug(void){
 }
 
 void calDist(nums num){
+    int firstX = 0, firstY = 0, secondX = 0, secondY = 0, xDist, yDist;
     printf("__startOfmakeArray__\n");
-    int result[num.big/4][4];
     for(int x = 0; x < num.big/4 + 1; x++){
         for(int y = 0; y < 4; y++){
-            result[x][y] = 4*x+y+1;
-            printf("%d, ", result[x][y]);
+            int result = 4*x+y+1;
+            if(result == num.big){
+                printf("First Value Found %d %d from %d\n", x, y, result);
+                firstX = x;
+                firstY = y;
+            }
+            if(result == num.small){
+                printf("Second Value Found %d %d from %d\n", x, y, result);
+                secondX = x;
+                secondY = y;
+            }
+            printf("%d, ", result);
         }
         printf("\n");
     }
     
     printf("%d | %d\n__endOfmakeArray__\n", num.big, num.small);
-
-    int firstX = 0, firstY = 0, secondX = 0, secondY = 0, xDist, yDist;
-    
-    for(int x = 0; x <= num.big/4; x++){
-        for(int y = 0; y < 4; y++){
-            if(result[x][y] == num.big){
-                printf("First Value Found %d %d from %d\n", x, y, result[x][y]);
-                firstX = x;
-                firstY = y;
-            }
-            if(result[x][y] == num.small){
-                printf("Second Value Found %d %d from %d\n", x, y, result[x][y]);
-                secondX = x;
-                secondY = y;
-            }
-        }
-    }
     
     printf("FV: %d & %d || SV: %d & %d\n", firstX, firstY, secondX, secondY);
     
